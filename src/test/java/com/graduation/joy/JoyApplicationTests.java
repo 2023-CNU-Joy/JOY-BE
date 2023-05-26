09b22f5e3af79b9f10d6901a5731104f6d097d02
hj-k66 <hjx66@naver.com>
2023-05-26 20:43:06 +0900

    initial project

---
+++ src/test/java/com/graduation/joy/JoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graduation.joy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
