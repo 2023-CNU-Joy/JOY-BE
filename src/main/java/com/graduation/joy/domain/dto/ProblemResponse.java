9b65cc1aebd4f00f42b2649dc4bfea69e62affc2
hj-k66 <hjx66@naver.com>
2023-05-27 20:49:19 +0900

    :ambulance: hoxfix: exampleTestInput, exampleTestOut List 반환으로 변경

--- src/main/java/com/graduation/joy/domain/dto/ProblemResponse.java
+++ src/main/java/com/graduation/joy/domain/dto/ProblemResponse.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,11 +15,15 @@ public class ProblemResponse {
     private Long id;
     private String title;
     private String content;
-    private String exampleTestInput;
-    private String exampleTestOut;
+    private List<String> exampleTestInput;
+    private List<String> exampleTestOut;
 
     public static ProblemResponse from(Problem problem) {
+        List<String> exampleTestInput = new ArrayList<>();
+        exampleTestInput.add(problem.getExampleTestInput());
+        List<String> exampleTestOutput = new ArrayList<>();
+        exampleTestOutput.add(problem.getExampleTestOut());
         return new ProblemResponse(problem.getId(), problem.getTitle(), problem.getContent(),
-                problem.getExampleTestInput(), problem.getExampleTestOut());
+                exampleTestInput, exampleTestOutput);
     }
 }
