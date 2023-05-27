dbe8c15cab52cd590551b0aab55132109cf4fa3e
hj-k66 <hjx66@naver.com>
2023-05-27 21:34:17 +0900

    :ambulance: hoxfix: testcase encrypt 방식 변경 >> list로

--- src/main/java/com/graduation/joy/utils/AESEncoder.java
+++ src/main/java/com/graduation/joy/utils/AESEncoder.java
@@ -5,36 +5,34 @@ import java.util.Base64;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.security.spec.AlgorithmParameterSpec;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AESEncoder {
 
     public static String encryptTestCaseList(String secretKey, List<TestCaseResponse> data) throws Exception{
-        AlgorithmParameterSpec ivSpec = new IvParameterSpec(new byte[16]);
         SecretKeySpec newKey = new SecretKeySpec(secretKey.getBytes("UTF-8"), "AES"); // [암호화 알고리즘]
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE,newKey,ivSpec);
+        cipher.init(Cipher.ENCRYPT_MODE,newKey);
 
-        List<String> encryptedData = new ArrayList<>();
+        List<List<List<String>>> encryptedData = new ArrayList<>();
         for(TestCaseResponse testCaseResponse : data){
-            String encryped = encryptTestCase(testCaseResponse, cipher);
+            List<List<String>> encryped = encryptTestCase(testCaseResponse, cipher);
             encryptedData.add(encryped);
         }
 
-        return String.join(",", encryptedData);
+        return encryptedData.toString();
 
     }
 
-    private static String encryptTestCase(TestCaseResponse testCaseResponse, Cipher cipher) throws IllegalBlockSizeException, BadPaddingException {
-        StringBuilder sb = new StringBuilder();
-        sb.append(testCaseResponse.getProblemId()).append(":");
-        sb.append(encryptList(testCaseResponse.getInputs(), cipher)).append(":");
-        sb.append(encryptList(testCaseResponse.getOutputs(), cipher)).append(":");
-        return sb.toString();
+    private static List<List<String>> encryptTestCase(TestCaseResponse testCaseResponse, Cipher cipher) throws IllegalBlockSizeException, BadPaddingException {
+        List<List<String>> encrypted = new ArrayList<>();
+        List<String> input = encryptList(testCaseResponse.getInputs(), cipher);
+        List<String> output = encryptList(testCaseResponse.getOutputs(), cipher);
+        encrypted.add(input);
+        encrypted.add(output);
+        return encrypted;
     }
 
     private static List<String> encryptList(List<String> data, Cipher cipher) throws IllegalBlockSizeException, BadPaddingException {
