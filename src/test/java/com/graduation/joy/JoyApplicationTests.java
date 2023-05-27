c5b4d59c8e2fe5c512d8ce623f16ed8abdcfd71c
hj-k66 <hjx66@naver.com>
2023-05-27 19:18:37 +0900

    :ambulance: hoxfix: test 주석처리

--- src/test/java/com/graduation/joy/JoyApplicationTests.java
+++ src/test/java/com/graduation/joy/JoyApplicationTests.java
@@ -3,11 +3,11 @@ package com.graduation.joy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class JoyApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class JoyApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
